10523fda6de453eb5344b026f682f966a312d30e
DamiaV <damia.vergnet@protonmail.com>
2025-08-11 03:13:32 +0200

    Add console.log with gadget name and version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+const version = "6.0";
+console.log(`Chargement de Gadget-wikt.create-new-entry (v${version})…`);
+
 createApp(App).mount("#app");
