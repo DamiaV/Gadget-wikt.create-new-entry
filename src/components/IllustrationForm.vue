775e280399f4617c43d795e86e42ec773d383715
DamiaV <damia.vergnet@protonmail.com>
2025-10-30 20:01:33 +0100

    Fix warning for IllustrationForm having no valid root node

--- src/components/IllustrationForm.vue
+++ src/components/IllustrationForm.vue
@@ -309,243 +309,245 @@ export default defineComponent({
 </script>
 
 <template>
-  <cdx-field class="cne-illustration-form cne-box" is-fieldset>
-    <template #label>
-      <cdx-icon :icon="cdxIconImageLayoutFrame"></cdx-icon>
-      Illustration
-      <span class="cne-fieldset-btns">
-        <wiki-link page-title="Aide:Illustrations">
-          <cdx-icon :icon="cdxIconHelpNotice"></cdx-icon>
-        </wiki-link>
+  <div>
+    <cdx-field class="cne-illustration-form cne-box" is-fieldset>
+      <template #label>
+        <cdx-icon :icon="cdxIconImageLayoutFrame"></cdx-icon>
+        Illustration
+        <span class="cne-fieldset-btns">
+          <wiki-link page-title="Aide:Illustrations">
+            <cdx-icon :icon="cdxIconHelpNotice"></cdx-icon>
+          </wiki-link>
 
-        <wiki-link page-title="Convention:Illustrations">
-          <cdx-icon :icon="cdxIconInfoFilled"></cdx-icon>
-        </wiki-link>
+          <wiki-link page-title="Convention:Illustrations">
+            <cdx-icon :icon="cdxIconInfoFilled"></cdx-icon>
+          </wiki-link>
 
-        <cdx-button
-          type="button"
-          size="small"
-          :aria-label="showFields ? 'Enrouler' : 'Dérouler'"
-          :title="showFields ? 'Enrouler' : 'Dérouler'"
-          @click="showFields = !showFields"
-        >
-          <cdx-icon
-            :icon="showFields ? cdxIconCollapse : cdxIconExpand"
-          ></cdx-icon>
-        </cdx-button>
+          <cdx-button
+            type="button"
+            size="small"
+            :aria-label="showFields ? 'Enrouler' : 'Dérouler'"
+            :title="showFields ? 'Enrouler' : 'Dérouler'"
+            @click="showFields = !showFields"
+          >
+            <cdx-icon
+              :icon="showFields ? cdxIconCollapse : cdxIconExpand"
+            ></cdx-icon>
+          </cdx-button>
 
-        <cdx-button
-          type="button"
-          size="small"
-          action="destructive"
-          aria-label="Supprimer"
-          title="Supprimer"
-          @click="onDelete"
-        >
-          <cdx-icon :icon="cdxIconTrash"></cdx-icon>
-        </cdx-button>
-      </span>
-    </template>
+          <cdx-button
+            type="button"
+            size="small"
+            action="destructive"
+            aria-label="Supprimer"
+            title="Supprimer"
+            @click="onDelete"
+          >
+            <cdx-icon :icon="cdxIconTrash"></cdx-icon>
+          </cdx-button>
+        </span>
+      </template>
 
-    <div v-show="showFields">
-      <div
-        v-show="
-          (type === 'image' && imageUrl) ||
-          (type === 'video' && videoSources.sources) ||
-          (type === 'audio' && audioSources.length) ||
-          (type === 'color' && color)
-        "
-        class="cne-preview-box"
-      >
-        Aperçu
-
-        <img
-          v-if="type === 'image'"
-          class="cne-image-preview"
-          :src="imageUrl"
-          :alt="alt"
-        />
-
-        <video
-          v-else-if="type === 'video'"
-          :key="videoSources"
-          :poster="videoSources.thumbUrl"
-          class="cne-image-preview"
-          controls
-        >
-          <source
-            v-for="(source, i) in videoSources.sources"
-            :key="i"
-            :src="source.src"
-            :type="source.type"
-          />
-          Impossible de lire la vidéo
-        </video>
-
-        <audio v-else-if="type === 'audio'" :key="audioSources" controls>
-          <source
-            v-for="(source, i) in audioSources"
-            :key="i"
-            :src="source.src"
-            :type="source.type"
-          />
-          Impossible de lire le fichier
-        </audio>
-
+      <div v-show="showFields">
         <div
+          v-show="
+            (type === 'image' && imageUrl) ||
+            (type === 'video' && videoSources.sources) ||
+            (type === 'audio' && audioSources.length) ||
+            (type === 'color' && color)
+          "
+          class="cne-preview-box"
+        >
+          Aperçu
+
+          <img
+            v-if="type === 'image'"
+            class="cne-image-preview"
+            :src="imageUrl"
+            :alt="alt"
+          />
+
+          <video
+            v-else-if="type === 'video'"
+            :key="videoSources"
+            :poster="videoSources.thumbUrl"
+            class="cne-image-preview"
+            controls
+          >
+            <source
+              v-for="(source, i) in videoSources.sources"
+              :key="i"
+              :src="source.src"
+              :type="source.type"
+            />
+            Impossible de lire la vidéo
+          </video>
+
+          <audio v-else-if="type === 'audio'" :key="audioSources" controls>
+            <source
+              v-for="(source, i) in audioSources"
+              :key="i"
+              :src="source.src"
+              :type="source.type"
+            />
+            Impossible de lire le fichier
+          </audio>
+
+          <div
+            v-else-if="type === 'color'"
+            class="cne-color-preview"
+            :style="{ backgroundColor: color }"
+          ></div>
+
+          <a
+            v-show="
+              (type === 'image' || type === 'video' || type === 'audio') &&
+              fileName
+            "
+            :href="`https://commons.wikimedia.org/wiki/File:${fileName}`"
+            target="_blank"
+            title="Voir le fichier sur Commons (S’ouvre dans un nouvel onglet)"
+            >Fichier</a
+          >
+        </div>
+
+        <cdx-radio
+          v-for="(type_, i) in types"
+          :key="i"
+          v-model="type"
+          :input-value="type_.value"
+          name="type"
+          @change="onTypeUpdate"
+        >
+          {{ type_.label }}
+        </cdx-radio>
+
+        <input-with-toolbar
+          v-if="type === 'image' || type === 'video' || type === 'audio'"
+          v-model="rawFileName"
+          :show-format-buttons="false"
+          :special-characters="[]"
+          :validator="fileNameValidator"
+          clearable
+          :required="!$props.userPreferences.formValidityCheckingDisabled"
+          @change="onFileNameUpdate"
+          @update:model-value="onInput"
+          @invalid="onInvalid"
+        >
+          <template #label>
+            Nom du fichier sur Commons
+            <span class="cne-fieldset-btns">
+              <a
+                :href="`https://commons.wikimedia.org/wiki/Special:MediaSearch?search=${encodeURIComponent(config.word)}&type=${type}`"
+                target="_blank"
+                :title="`Chercher «\u00a0${config.word}\u00a0» sur Commons (S’ouvre dans un nouvel onglet)`"
+              >
+                <cdx-icon :icon="cdxIconSearch"></cdx-icon> </a
+            ></span>
+          </template>
+        </input-with-toolbar>
+
+        <cdx-field
+          v-else-if="type === 'text'"
+          :status="status"
+          :messages="messages"
+        >
+          <template #label>Texte</template>
+          <cdx-text-input
+            v-model.trim="text"
+            class="wikitext"
+            clearable
+            :required="!$props.userPreferences.formValidityCheckingDisabled"
+            @change="fireUpdateEvent"
+            @update:model-value="onInput"
+            @invalid="onInvalid"
+          ></cdx-text-input>
+        </cdx-field>
+
+        <cdx-field
           v-else-if="type === 'color'"
+          :status="status"
+          :messages="messages"
+        >
+          <template #label>Code de la couleur</template>
+          <template #description>Un code couleur CSS.</template>
+          <cdx-text-input
+            v-model.trim="color"
+            clearable
+            :required="!$props.userPreferences.formValidityCheckingDisabled"
+            @change="fireUpdateEvent"
+            @update:model-value="onInput"
+            @invalid="onInvalid"
+          ></cdx-text-input>
+        </cdx-field>
+
+        <input-with-toolbar
+          v-model="description"
+          clearable
+          :required="!$props.userPreferences.formValidityCheckingDisabled"
+          @change="fireUpdateEvent"
+        >
+          <template #label>Légende</template>
+          <template #description>
+            Une courte description de l’illustration.
+          </template>
+          <template #help-text>
+            Si le mot illustré est présent dans la légende, il doit être en
+            <strong>gras</strong>.
+          </template>
+        </input-with-toolbar>
+
+        <cdx-field
+          v-if="type === 'image' || type === 'video' || type === 'audio'"
+        >
+          <template #label>Texte alternatif</template>
+          <template #description>
+            Texte qui sera affiché si le fichier ne peut pas être chargé.
+          </template>
+          <cdx-text-input
+            v-model.trim="alt"
+            clearable
+            @change="fireUpdateEvent"
+          ></cdx-text-input>
+          <template #help-text>
+            Il est fortement recommandé de renseigner un texte alternatif pour
+            une meilleure accessibilité.
+          </template>
+        </cdx-field>
+      </div>
+      <collapsed-preview
+        v-show="!showFields"
+        :text="
+          (type === 'image' && 'Image\u00a0: ' + fileName) ||
+          (type === 'video' && 'Vidéo\u00a0: ' + fileName) ||
+          (type === 'audio' && 'Fichier audio\u00a0: ' + fileName) ||
+          (type === 'text' && 'Texte\u00a0: ' + text) ||
+          (type === 'color' && 'Couleur\u00a0: ' + color)
+        "
+      >
+        <div
+          v-show="type === 'color'"
           class="cne-color-preview"
           :style="{ backgroundColor: color }"
         ></div>
+      </collapsed-preview>
+    </cdx-field>
 
-        <a
-          v-show="
-            (type === 'image' || type === 'video' || type === 'audio') &&
-            fileName
-          "
-          :href="`https://commons.wikimedia.org/wiki/File:${fileName}`"
-          target="_blank"
-          title="Voir le fichier sur Commons (S’ouvre dans un nouvel onglet)"
-          >Fichier</a
-        >
-      </div>
-
-      <cdx-radio
-        v-for="(type_, i) in types"
-        :key="i"
-        v-model="type"
-        :input-value="type_.value"
-        name="type"
-        @change="onTypeUpdate"
-      >
-        {{ type_.label }}
-      </cdx-radio>
-
-      <input-with-toolbar
-        v-if="type === 'image' || type === 'video' || type === 'audio'"
-        v-model="rawFileName"
-        :show-format-buttons="false"
-        :special-characters="[]"
-        :validator="fileNameValidator"
-        clearable
-        :required="!$props.userPreferences.formValidityCheckingDisabled"
-        @change="onFileNameUpdate"
-        @update:model-value="onInput"
-        @invalid="onInvalid"
-      >
-        <template #label>
-          Nom du fichier sur Commons
-          <span class="cne-fieldset-btns">
-            <a
-              :href="`https://commons.wikimedia.org/wiki/Special:MediaSearch?search=${encodeURIComponent(config.word)}&type=${type}`"
-              target="_blank"
-              :title="`Chercher «\u00a0${config.word}\u00a0» sur Commons (S’ouvre dans un nouvel onglet)`"
-            >
-              <cdx-icon :icon="cdxIconSearch"></cdx-icon> </a
-          ></span>
-        </template>
-      </input-with-toolbar>
-
-      <cdx-field
-        v-else-if="type === 'text'"
-        :status="status"
-        :messages="messages"
-      >
-        <template #label>Texte</template>
-        <cdx-text-input
-          v-model.trim="text"
-          class="wikitext"
-          clearable
-          :required="!$props.userPreferences.formValidityCheckingDisabled"
-          @change="fireUpdateEvent"
-          @update:model-value="onInput"
-          @invalid="onInvalid"
-        ></cdx-text-input>
-      </cdx-field>
-
-      <cdx-field
-        v-else-if="type === 'color'"
-        :status="status"
-        :messages="messages"
-      >
-        <template #label>Code de la couleur</template>
-        <template #description>Un code couleur CSS.</template>
-        <cdx-text-input
-          v-model.trim="color"
-          clearable
-          :required="!$props.userPreferences.formValidityCheckingDisabled"
-          @change="fireUpdateEvent"
-          @update:model-value="onInput"
-          @invalid="onInvalid"
-        ></cdx-text-input>
-      </cdx-field>
-
-      <input-with-toolbar
-        v-model="description"
-        clearable
-        :required="!$props.userPreferences.formValidityCheckingDisabled"
-        @change="fireUpdateEvent"
-      >
-        <template #label>Légende</template>
-        <template #description>
-          Une courte description de l’illustration.
-        </template>
-        <template #help-text>
-          Si le mot illustré est présent dans la légende, il doit être en
-          <strong>gras</strong>.
-        </template>
-      </input-with-toolbar>
-
-      <cdx-field
-        v-if="type === 'image' || type === 'video' || type === 'audio'"
-      >
-        <template #label>Texte alternatif</template>
-        <template #description>
-          Texte qui sera affiché si le fichier ne peut pas être chargé.
-        </template>
-        <cdx-text-input
-          v-model.trim="alt"
-          clearable
-          @change="fireUpdateEvent"
-        ></cdx-text-input>
-        <template #help-text>
-          Il est fortement recommandé de renseigner un texte alternatif pour une
-          meilleure accessibilité.
-        </template>
-      </cdx-field>
-    </div>
-    <collapsed-preview
-      v-show="!showFields"
-      :text="
-        (type === 'image' && 'Image\u00a0: ' + fileName) ||
-        (type === 'video' && 'Vidéo\u00a0: ' + fileName) ||
-        (type === 'audio' && 'Fichier audio\u00a0: ' + fileName) ||
-        (type === 'text' && 'Texte\u00a0: ' + text) ||
-        (type === 'color' && 'Couleur\u00a0: ' + color)
-      "
+    <cdx-dialog
+      v-model:open="openDeletionDialog"
+      title="Confirmation de suppression"
+      use-close-button
+      :primary-action="dialogPrimaryAction"
+      :default-action="dialogDefaultAction"
+      @primary="deleteIllustration"
+      @default="openDeletionDialog = false"
     >
-      <div
-        v-show="type === 'color'"
-        class="cne-color-preview"
-        :style="{ backgroundColor: color }"
-      ></div>
-    </collapsed-preview>
-  </cdx-field>
-
-  <cdx-dialog
-    v-model:open="openDeletionDialog"
-    title="Confirmation de suppression"
-    use-close-button
-    :primary-action="dialogPrimaryAction"
-    :default-action="dialogDefaultAction"
-    @primary="deleteIllustration"
-    @default="openDeletionDialog = false"
-  >
-    Êtes-vous
-    {{ userGenderSwitch(config.userGender, "sûr·e", "sûre", "sûr") }} de vouloir
-    supprimer cette illustration&nbsp;?
-    <template #footer-text>Cette action est irréversible.</template>
-  </cdx-dialog>
+      Êtes-vous
+      {{ userGenderSwitch(config.userGender, "sûr·e", "sûre", "sûr") }} de
+      vouloir supprimer cette illustration&nbsp;?
+      <template #footer-text>Cette action est irréversible.</template>
+    </cdx-dialog>
+  </div>
 </template>
 
 <style>
